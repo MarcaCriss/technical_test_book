674eca6dea2a77992ef0752d1a914062583b9512
Criss_Mark_ <cristianmarca70@gmail.com>
2023-09-09 12:03:37 -0400

    feat: add pages and books module

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {
+        path: 'books',
+        loadChildren: () =>
+          import('./books/books.module').then((m) => m.BooksModule),
+      }
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {}
